bb1fa0723c73ec0c10b186e8339b8cf13f8ec6e3
DAVID J RHYNE <davidrhyne@att.net>
2021-06-23 17:12:51 -0500

    adding javascript for menu and 80% of mobile styling completed

---
+++ dist/src/sunnyside.js
@@ -0,0 +1,30 @@
+const menu = document.getElementById('nav-toggle')
+const nav = document.getElementById('nav')
+const headerTitle = document.getElementById('header__title')
+const headerArrow = document.getElementById('header__arrow')
+const hamburger = document.getElementById('hamburger')
+const menuItems = document.querySelectorAll('.menu-item')
+
+let isMenuOpen = false;
+
+function handleMenuClick() {
+    // hide these elements while the menu is open
+    nav.classList.toggle('hide')
+    headerTitle.classList.toggle('hide')
+    headerArrow.classList.toggle('hide')
+    // toggle isMenuOpen to true
+    isMenuOpen = !isMenuOpen;
+    // adjust the height of the hamburger
+    isMenuOpen === false ? hamburger.style.height = '2px' : hamburger.style.height = '1px';
+}
+
+// event listeners 
+// for opening and showing menu
+menu.addEventListener('click', handleMenuClick)
+
+// for hiding the menu after an option was selected
+menuItems.forEach(el => el.addEventListener('click', handleMenuClick))
+
+
+// on load
+nav.classList.add('hide')
